fb56c79b9ba0700bb6bd4f09b2a4291bcf9e19d6
Lukas Nilsson <lpn5765@uncw.edu>
2025-01-23 10:28:16 -0500

    Updated sha256.py script.

--- sha256.py
+++ sha256.py
@@ -14,7 +14,7 @@ with log.progress("Attempting to back: {}! \n".format(wanted_hash)) as p:
     with open(password_file, 'r', encoding='latin-1') as password_list:
         for password in password_list:
             password = password.strip("\n").encode('latin-1')
-            password_hash = sha256sum(password)
+            password_hash = sha256sumhex(password)
             p.status("[{}] {} == {}".format(attempts, password.decode('latin-1'), password_hash))
             if password_hash == wanted_hash:
                 p.success("Password hash found after {} attempts! {} hashes to {}!".format(attempts, password.decode('latin-1'), password_hash))
